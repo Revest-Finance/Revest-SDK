d89774f511cec690b64af9d3e681038e428b0789
RobAnon <rhmontgomery13@gmail.com>
2022-02-07 14:27:12 -0500

    Added in methods to simplify fetching a list of FNFTs using multicall and the graph

--- RevestSDK.js
+++ RevestSDK.js
@@ -1,5 +1,4 @@
 import { BigNumber, ethers, utils } from 'ethers';
-import { subgraphRequest } from "./RevestSDK.js";
 import { jsonToGraphQLQuery } from 'json-to-graphql-query';
 import addresses from './addresses.js';
 import {abi as RouterABI} from './abis/AddressRegistry.json';
@@ -7,6 +6,67 @@ import {abi as RouterABI} from './abis/AddressRegistry.json';
 export const SUBGRAPH_URL = {
   1: 'https://api.thegraph.com/subgraphs/name/alexvorobiov/eip1155subgraph'
 };
+
+export async function getFNFTsForUserAndContractWithURI(user, contractAddress, provider) {
+  const fnftHandlerABI = [
+    "function uri(uint fnftId) external view returns (string memory)",
+  ]
+
+  let net = await provider.getNetwork();
+  let chainId = net.chainId;
+  let allFNFTs = await getFNFTsForUserAndContract(user, contractAddress, provider);
+
+  let ids = allFNFTsForUser.ids;
+
+  let response = await multicall(
+    chainId,
+    provider,
+    abi,
+    ids.map((id) => [allFNFTs.nftAddress, 'uri', [id]])
+  );
+
+  let fnfts = {};
+  response.forEach((entry, index) => {
+    fnfts[ids[index]] = {uri: entry[0]};
+  });
+
+  allFNFTs.fnfts = fnfts;
+  return allFNFTs;
+
+  
+}
+
+export async function getFNFTsForUserAndContract(user, contractAddress, provider) {
+  const tokenVaultABI = [
+    "function getFNFT(uint fnftId) external view returns (tuple(address asset, address pipeToContract, uint depositAmount, uint depositMul, uint split, uint depositStopTime, bool maturityExtension, bool isMulti, bool nontransferrable))"
+  ];
+  let net = await provider.getNetwork();
+  let chainId = net.chainId;
+  let allFNFTsForUser = await getAllFNFTsForUser(user, provider);
+  let ids = allFNFTsForUser.ids;
+
+  const revestRouter = new ethers.Contract(addresses[chainId].ROUTER, RouterABI, provider);
+  const TOKEN_VAULT = await revestRouter.getTokenVault();
+
+  let response = await multicall(
+    chainId,
+    provider,
+    abi,
+    ids.map((id) => [TOKEN_VAULT, 'getFNFT', [id]])
+  );
+
+  let idsForContract = [];
+  response.forEach((entry, index) => {
+    if(entry[0].pipeToContract.toLowerCase() == contractAddress.toLowerCase()){
+      idsForContract.push(ids[index]);
+    }
+  })
+
+  allFNFTsForUser.ids = idsForContract;
+  allFNFTsForUser.contractAddress = contractAddress;
+  allFNFTsForUser.vaultAddress = TOKEN_VAULT;
+  return allFNFTsForUser;
+}
 
 export async function getAllFNFTsForUser(user, provider) {
 
@@ -49,6 +109,8 @@ export async function getAllFNFTsForUser(user, provider) {
   return {nftAddress:FNFT_HANDLER, ids: userFNFTs};
 }
 
+// Lower level helper methods
+
 export async function subgraphRequest(url, query, options = {}) {
     const res = await fetch(url, {
       method: 'POST',
@@ -61,4 +123,51 @@ export async function subgraphRequest(url, query, options = {}) {
     });
     const { data } = await res.json();
     return data || {};
+}
+
+export async function multicall(
+  network,
+  provider,
+  abi,
+  calls,
+  options= {}
+) {
+  const multicallAbi = [
+    'function aggregate(tuple(address target, bytes callData)[] calls) view returns (uint256 blockNumber, bytes[] returnData)'
+  ];
+
+  let net = await provider.getNetwork();
+  let chainId = net.chainId;
+
+  const multi = new ethers.Contract(
+    addresses[chainId].multicall,
+    multicallAbi,
+    provider
+  );
+
+  const itf = new ethers.Interface(abi);
+  try {
+    const max = options?.limit || 500;
+    const pages = Math.ceil(calls.length / max);
+    const promises = [];
+    Array.from(Array(pages)).forEach((x, i) => {
+      const callsInPage = calls.slice(max * i, max * (i + 1));
+      promises.push(
+        multi.aggregate(
+          callsInPage.map((call) => [
+            call[0].toLowerCase(),
+            itf.encodeFunctionData(call[1], call[2])
+          ]),
+          options || {}
+        )
+      );
+    });
+    let results = await Promise.all(promises);
+    results = results.reduce((prev, [, res]) => prev.concat(res), []);
+    return results.map((call, i) =>
+      itf.decodeFunctionResult(calls[i][1], call)
+    );
+  } catch (e) {
+    return Promise.reject(e);
+  }
 }
